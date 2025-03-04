4908d305de6f275102152711f903de153ea0558c
NORMss <serega.b301@gmail.com>
2025-03-05 01:49:41 +0700

    feat: all logic for working with data in firebase has been created

--- app/src/main/java/ru/normno/myfirebasefirestoreandroid/FirebaseClient.kt
+++ app/src/main/java/ru/normno/myfirebasefirestoreandroid/FirebaseClient.kt
@@ -1,8 +1,12 @@
 package ru.normno.myfirebasefirestoreandroid
 
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.launch
 
 class FirebaseClient {
     private val tag: String = "FirebaseClient"
@@ -13,15 +17,73 @@ class FirebaseClient {
         return callbackFlow {
             db.collection(collection)
                 .add(user.toHashMap())
+                .addOnSuccessListener { document ->
+                    println(tag + "insert user with id: ${document.id}")
+                    CoroutineScope(Dispatchers.IO).launch {
+                        updateUser(user.copy(id = document.id)).collect {}
+                    }
+                    trySend(document.id)
+                }
+                .addOnFailureListener { error ->
+                    error.printStackTrace()
+                    println(tag + "insert user failed: ${error.message}")
+                    trySend(null)
+                }
+            awaitClose {
+                println(tag + "insert user close")
+            }
         }
     }
 
-    fun updateUser(): Flow<User?> {
-        TODO()
+    fun updateUser(user: User): Flow<Boolean> {
+        return callbackFlow {
+            db.collection(collection)
+                .document(user.id)
+                .set(user.toHashMap())
+                .addOnSuccessListener { document ->
+                    println(tag + "update user with id: ${user.id}")
+                    trySend(true)
+                }
+                .addOnFailureListener { error ->
+                    error.printStackTrace()
+                    println(tag + "updating user failed: ${error.message}")
+                    trySend(false)
+                }
+            awaitClose {
+                println(tag + "insert user close")
+            }
+        }
     }
 
-    fun getUser() {
-        TODO()
+    fun getUser(
+        email: String,
+    ): Flow<User?> {
+        return callbackFlow {
+            db.collection(collection)
+                .get()
+                .addOnSuccessListener { result ->
+                    var user: User? = null
+                    result.forEach { document ->
+                        if (document.get("email") == email) {
+                            user = document.data.toUser()
+                            println(tag + "user found with id: ${document.id}")
+                            trySend(user)
+                        }
+                    }
+                    if (user == null) {
+                        println(tag + "user not found with email: $email")
+                    }
+                }
+                .addOnFailureListener { error ->
+                    error.printStackTrace()
+                    println(tag + "getting user failed: ${error.message}")
+                    trySend(null)
+                }
+            awaitClose {
+                println(tag + "insert user close")
+            }
+
+        }
     }
 
     private fun User.toHashMap(): HashMap<String, Any> {
@@ -38,7 +100,7 @@ class FirebaseClient {
             id = get("id") as String,
             name = get("name") as String,
             email = get("email") as String,
-            age = get("age") as Int,
+            age = (get("age") as Long).toInt(),
         )
     }
 }
--- app/src/main/java/ru/normno/myfirebasefirestoreandroid/MainActivity.kt
+++ app/src/main/java/ru/normno/myfirebasefirestoreandroid/MainActivity.kt
@@ -4,26 +4,129 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.core.text.isDigitsOnly
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
 import ru.normno.myfirebasefirestoreandroid.ui.theme.MyFirebaseFirestoreAndroidTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        val firebaseClient = FirebaseClient()
+
         setContent {
             MyFirebaseFirestoreAndroidTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
+                    var localUser by remember {
+                        mutableStateOf(
+                            User(
+                                id = "",
+                                name = "Sergey",
+                                email = "serega.b301@gmail.com",
+                                age = 22,
+                            )
+                        )
+                    }
+
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center,
+                    ) {
+                        TextField(
+                            value = localUser.name,
+                            onValueChange = { localUser = localUser.copy(name = it) },
+                            label = { Text(text = "Name") }
+                        )
+                        TextField(
+                            value = localUser.email,
+                            onValueChange = { localUser = localUser.copy(email = it) },
+                            label = { Text(text = "Email") }
+                        )
+                        TextField(
+                            value = localUser.age.toString(),
+                            onValueChange = {
+                                if (it.isDigitsOnly()) {
+                                    localUser = localUser.copy(age = it.toInt())
+                                } else {
+                                    Unit
+                                }
+                            },
+                            label = { Text(text = "Age") },
+                            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                        )
+                        Button(
+                            onClick = {
+                                lifecycleScope.launch {
+                                    firebaseClient.insertUser(localUser).collect { id ->
+                                        localUser = localUser.copy(id = id ?: "")
+                                    }
+                                }
+                            }
+                        ) {
+                            Text(
+                                text = "Insert"
+                            )
+                        }
+                        Button(
+                            onClick = {
+                                localUser = localUser.copy(email = "serega.b302@yanex.ru")
+                                lifecycleScope.launch {
+                                    firebaseClient.updateUser(localUser).collect { result ->
+                                        if (result) {
+                                            println("user updated")
+                                        } else {
+                                            println("user not updated")
+                                        }
+                                    }
+                                }
+                            }
+                        ) {
+                            Text(
+                                text = "Update"
+                            )
+                        }
+                        Button(
+                            onClick = {
+                                lifecycleScope.launch {
+                                    firebaseClient.getUser(localUser.email).collect { user ->
+                                        if (user != null) {
+                                            localUser = user
+                                            println("user found: $user")
+                                        } else {
+                                            println("user not found")
+                                        }
+                                    }
+                                }
+                            }
+                        ) {
+                            Text(
+                                text = "Get User"
+                            )
+                        }
+                    }
                 }
             }
         }
