546d0596eda5cb7a541d2cf38c540815aa5d818b
NORMss <serega.b301@gmail.com>
2025-03-04 23:57:15 +0700

    feat: connected to firebase

--- app/src/main/java/ru/normno/myfirebasefirestoreandroid/FirebaseClient.kt
+++ app/src/main/java/ru/normno/myfirebasefirestoreandroid/FirebaseClient.kt
@@ -1,7 +1,6 @@
 package ru.normno.myfirebasefirestoreandroid
 
 import com.google.firebase.firestore.FirebaseFirestore
-import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
 
@@ -10,18 +9,36 @@ class FirebaseClient {
     private val db: FirebaseFirestore = FirebaseFirestore.getInstance()
     private val collection: String = "user"
 
-    fun insertUser(): Flow<String?> {
+    fun insertUser(user: User): Flow<String?> {
         return callbackFlow {
-            trySend(null)
-            awaitClose()
+            db.collection(collection)
+                .add(user.toHashMap())
         }
     }
 
-    fun updateUser(): Flow<User?>{
+    fun updateUser(): Flow<User?> {
         TODO()
     }
 
-    fun getUser(){
+    fun getUser() {
         TODO()
+    }
+
+    private fun User.toHashMap(): HashMap<String, Any> {
+        return hashMapOf(
+            "id" to id,
+            "name" to name,
+            "email" to email,
+            "age" to age,
+        )
+    }
+
+    private fun Map<String, Any>.toUser(): User {
+        return User(
+            id = get("id") as String,
+            name = get("name") as String,
+            email = get("email") as String,
+            age = get("age") as Int,
+        )
     }
 }
